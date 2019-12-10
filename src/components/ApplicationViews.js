89f5904881280244a63f32766f1abec0611c2650
Caroline Brownlee <brownleecaroline@gmail.com>
2019-12-10 13:41:54 -0600

    fixed bug in app views redirecting to the wrong root upon login

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import React, { Component } from 'react'
 import UploadPrintForm from './Prints/UploadPrintForm';
 import LoginForm from './Authentication/LoginForm';
@@ -9,11 +9,22 @@ class ApplicationViews extends Component {
         return (
             <React.Fragment>
                 <Route exact path="/" render={(props) => {
-                    return <LoginForm setUser={this.props.setUser} {...props}/>
+                if (this.props.user) {
+                    return <Redirect to="/prints/new" {...props} {...this.props} />
+                } else {
+                    return <LoginForm setUser={this.props.setUser} {...props} />
+                }
                 }} />
-                <Route path="/prints/new" render={(props) => {
+                <Route exact path="/prints" render={(props) => {
+                if (this.props.user) {
+                    return <Redirect to="/prints" {...props} {...this.props} />
+                } else {
+                    return <LoginForm setUser={this.props.setUser} {...props} />
+                }
+                }} />
+                {/* <Route path="/prints/new" render={(props) => {
                     return <UploadPrintForm {...props} />
-                }} />
+                }} /> */}
             </React.Fragment>
         )
     }
