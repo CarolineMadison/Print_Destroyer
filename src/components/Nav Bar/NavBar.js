151c89ec445d7fecc109047a1e2ddfda38f568eb
Caroline Brownlee <brownleecaroline@gmail.com>
2019-12-12 07:32:54 -0600

    finished styling on a few things

--- src/components/Nav Bar/NavBar.js
+++ src/components/Nav Bar/NavBar.js
@@ -2,8 +2,31 @@ import React, { Component } from 'react';
 import { Link, withRouter } from "react-router-dom"
 import './NavBar.css'
 import { isUserWhitespacable } from '@babel/types';
+import printAPIManager from '../../modules/printAPIManager';
 
 class NavBar extends Component {
+
+  state = {
+    user: {
+      isAdmin: false
+    }
+  }
+
+  componentDidMount() {
+    const userId = localStorage.getItem("credentials")
+    console.log(userId)
+    // get all users from database
+    printAPIManager.getAll("users")
+      .then((users) => {
+        // array method to find adminstrator in users array and store them into a variable
+        const loggedInUser = users.find(user => user.id === Number(userId))
+        console.log(loggedInUser)
+        // set user to administrator
+        this.setState({
+          user: loggedInUser
+        })
+      })
+  }
 
   handleLogout = () => {
     console.log(this.props.history)
@@ -16,17 +39,18 @@ class NavBar extends Component {
     return (
       <header>
         <div className="headerContainer">
-          {/* <picture>
-            <img className="logoImage" src={require('./printDestroyerMainLogo.png')} alt="Print Destroyer Logo" />
-          </picture> */}
           <nav className="navBar">
             <ul className="navList">
               {(this.props.user) ?
                 <>
+          {/* <picture>
+            <img className="logoImage" src={require('./printDestroyerMainLogo.png')} alt="Print Destroyer Logo" />
+          </picture> */}
                   <li><Link className="nav-link" to="/profile">Profile</Link></li>
                   <li><Link className="nav-link" to="/prints">Prints</Link></li>
                   <li><Link className="nav-link" to="/customers">Wish List</Link></li>
                   <li><Link className="nav-link" onClick={this.handleLogout}>Log Out</Link></li>
+                  <hr />
                 </>
                 : null
               }
